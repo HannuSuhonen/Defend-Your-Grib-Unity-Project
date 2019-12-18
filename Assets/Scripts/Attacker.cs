84517fcb707164e087a95113f05ce701d1a80a92
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-18 16:08:54 +0200

    Added Attacker Spawn script

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [SerializeField] [Range(0,10)] float walkingSpeed = 1f;
+    [SerializeField] AnimationClip spawnAnimation;
+
+    void Update()
+    {
+        StartCoroutine(Walk());
+    }
+
+    private IEnumerator Walk()
+    {
+        if (spawnAnimation != null) //checks to see if spawn animation exists. 
+        {
+            yield return new WaitForSeconds(spawnAnimation.length);
+        }
+        transform.Translate(Vector2.left * Time.deltaTime * walkingSpeed);
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker enemyPrefab; //using Attacker Class here as its assigned to the revelant prefab. 
+    [SerializeField] bool spawn = true;
+    [SerializeField] float min = 1f;
+    [SerializeField] float max = 5f;
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(min, max));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+}
