257647fdc5b81ac6cc861e6d2934b04b3be1c444
Hannu Suhonen <han.suhonen@icloud.com>
2020-01-04 13:52:43 +0200

    Added enemy attack logic.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0,10)] float currentSpeed = 1f; //this attribute can be changed in the animation event.  
-
+    GameObject currentTarget;
+    
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
@@ -14,5 +15,12 @@ public class Attacker : MonoBehaviour
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("IsAttacking",true);
+        currentTarget = target;
+        Debug.Log("I'm attacking");
     }
 }
---
+++ Assets/Scripts/Juju.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juju : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        GameObject target = other.gameObject;
+        var defender = target.GetComponent<Defender>();
+        if(target.GetComponent<Defender>())
+        {
+            Debug.Log("touch defender");
+            GetComponent<Attacker>().Attack(target);
+        }
+        
+
+    }
+}
