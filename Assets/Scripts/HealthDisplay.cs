b62e0f27b50548c9b49f37b3cdbdd0c16e902b9f
Hannu Suhonen <han.suhonen@icloud.com>
2020-01-07 14:30:50 +0200

    Created end game logic and player health.

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    Text healthDisplay;
+    int health = 20;
+    int healthLoss = 10;
+    int sceneLoadDelay = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthDisplay = GetComponent<Text>();
+        healthDisplay.text = health.ToString();
+    }
+
+    public void HealthSubtract()
+    {
+        health -= healthLoss;
+        healthDisplay.text = health.ToString();
+        StartCoroutine(LoadStartScene());
+    }
+
+    IEnumerator LoadStartScene()
+    {
+        if(health <= 0)
+        {
+            yield return new WaitForSeconds(sceneLoadDelay);
+            SceneManager.LoadScene(1);
+        }
+    }
+}
--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shredder : MonoBehaviour
 {
+    HealthDisplay healthDisplay;
+    private void Start() 
+    {
+        healthDisplay = FindObjectOfType<HealthDisplay>();
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        healthDisplay.HealthSubtract();
     }
 }
