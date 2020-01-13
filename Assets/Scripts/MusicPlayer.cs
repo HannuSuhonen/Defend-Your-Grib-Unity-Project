6c1736d3fd20dd4f899e12d8ce27fe3f247511d0
Hannu Suhonen <han.suhonen@icloud.com>
2020-01-14 01:16:02 +0200

    Added functionality to options menu.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        SetUpSingleton();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    private void SetUpSingleton()
+    {
+        int NumberOfMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if(NumberOfMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    private void Start() 
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    private void Update() 
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if(musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No musicplayer found, did you start from Start Menu?");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
