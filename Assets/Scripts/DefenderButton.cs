e4ad3fa4d177105c3742ec7d4ad97085638a963d
Hannu Suhonen <han.suhonen@icloud.com>
2020-01-16 14:58:05 +0200

    Added Star cost text component to

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
 
     [SerializeField] Defender defenderPrefab;
+
+    private void Start() 
+    {
+        LabelDefenderButton();
+    }
+
+    private void LabelDefenderButton()
+    {
+        Text starCost = GetComponentInChildren<Text>();
+        if(!starCost)
+        {
+            Debug.LogError(name + " :check star cost text");
+        }
+        else
+        {
+            starCost.text = defenderPrefab.GetStarCost().ToString();
+        }
+    }
 
     private void OnMouseDown()
     {
